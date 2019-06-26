193986b6649cb5d0d9e97aa88c7f7a85154414b5
Bryan Roof <broof716@gmail.com>
2019-06-26 16:57:21 +0000

    ImageBlur1 update

--- ImageBlur1.rb
+++ ImageBlur1.rb
@@ -4,17 +4,42 @@ class Image
   end
 
   def output_image
-    @image.each do |x|
-      puts x.join(" ")
-    end
+    puts @image.class
+     @image.each do |x|
+       puts x.join("")
+     end
   end
 end
 
 image = Image.new([
+  [0, 0, 1, 0],
   [0, 0, 0, 0],
-  [0, 1, 0, 0],
-  [0, 0, 0, 1],
-  [0, 0, 0, 0]
+  [0, 0, 0, 0],
+  [1, 0, 0, 0]
 ])
 
 image.output_image
+
+
+#------WORKING CODE BELOW
+# class Image
+#   def initialize(image)
+#     @image = image
+#   end
+
+#   def output_image
+#     @image.each do |x|
+#       puts x.join(" ")
+#     end
+#   end
+# end
+
+# image = Image.new([
+#   [0, 0, 0, 0],
+#   [0, 1, 0, 0],
+#   [0, 0, 0, 1],
+#   [0, 0, 0, 0]
+# ])
+
+# image.output_image
+#------------END OF WORKING CODe
